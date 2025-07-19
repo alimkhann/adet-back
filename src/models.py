854279d71a0f87e3e486d316315b6656fc7a698b
alim <alimkhan.ergebayev@gmail.com>
2025-07-19 18:22:19 +0500

    models

--- src/models.py
+++ src/models.py
@@ -1,19 +1,19 @@
 from sqlalchemy.sql import func
 
-from .database import Base
+from src.database import Base
 
 # Import all models to register them with SQLAlchemy Base
-from .auth.models import User
-from .onboarding.models import OnboardingAnswer
-from .habits.models import (
+from src.auth.models import User
+from src.onboarding.models import OnboardingAnswer
+from src.habits.models import (
     Habit,
     MotivationEntry,
     AbilityEntry,
     TaskEntry,
     TaskValidation
 )
-from .friends.models import Friendship, FriendRequest, CloseFriend, BlockedUser, UserReport
-from .chats.models import Conversation, Message, ConversationParticipant
-from .posts.models import Post, PostComment, PostLike, PostView, PostReport
-from .support.models import SupportRequest, BugReport, SupportResponse
+from src.friends.models import Friendship, FriendRequest, CloseFriend, BlockedUser, UserReport
+from src.chats.models import Conversation, Message, ConversationParticipant
+from src.posts.models import Post, PostComment, PostLike, PostView, PostReport
+from src.support.models import SupportRequest, BugReport, SupportResponse
 
